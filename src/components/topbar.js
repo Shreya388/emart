8ef17b29763e840551f623bf0a14d2378ce30d5c
Shreya388 <meenaxjha@gmail.com>
2023-10-21 15:56:46 +0530

    added searbox in navbar

--- src/components/topbar.js
+++ src/components/topbar.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 import "./topbar.css";
-import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, Container, NavDropdown, Form, FormControl, Button } from "react-bootstrap";
 
 const Topbar = () => {
   const [expanded, setExpanded] = useState(false);
+
+  const [showSearchForm, setShowSearchForm] = useState(false);
+
+  const toggleSearchForm = () => {
+    setShowSearchForm(!showSearchForm);
+  };
 
   return (
     <>
@@ -19,10 +25,10 @@ const Topbar = () => {
             <Nav className="mx-auto">
               <Nav.Link className="alink" href="/">Home</Nav.Link>
               <NavDropdown className="link" title="Shop" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.4">More items</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">Chair</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Tables</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Bucket</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">Bed</NavDropdown.Item>
               </NavDropdown>
               <NavDropdown className="link" title="Product" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
@@ -35,12 +41,24 @@ const Topbar = () => {
               <Nav.Link className="link" href="/about">About Us</Nav.Link>
               <Nav.Link className="link" href="/contact">Contact Us</Nav.Link>
             </Nav>
-            
+
+            {showSearchForm && (
+              <form action="/search" method="get">
+                <input type="text" id="search" className="nav-search" name="query" placeholder="search term" required />
+                
+              </form>
+            )}
+            <div className="search-icon" onClick={toggleSearchForm}>
               <big><i class="fa fa-solid fa-magnifying-glass pr-4"></i></big>
-              <big><i class="fa fa-regular fa-user pr-4"></i></big>
-              <big><i class="fa fa-regular fa-heart pr-4"></i></big>
-              <big><i class="fa fa-solid fa-cart-shopping"></i></big>
+            </div>
+
+
+            {/* ... */}
+            <big><i class="fa fa-regular fa-user pr-4"></i></big>
+            <big><i class="fa fa-regular fa-heart pr-4"></i></big>
+            <big><i class="fa fa-solid fa-cart-shopping"></i></big>
           </Navbar.Collapse>
+
         </Container>
       </Navbar>
     </>
